29848b1b8dcde534a46a4a05ea693dd47834db2c
Bethany C <bethanylyc@gmail.com>
2022-10-21 00:15:09 -0400

    initial base react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import PDFViewer from './components/PDFViewer/PDFViewer';
-import PDFJSBackend from './backends/pdfjs';
-import SemanticScholarAPI from './backends/SemanticScholarAPI';
-import NavBar from './components/NavBar';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        {/* <PDFViewer 
-          backend={ PDFJSBackend }
-          src='/2022604_Draft_Proposal.pdf'
-        /> */}
-        <NavBar/>
-      <SemanticScholarAPI/>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
