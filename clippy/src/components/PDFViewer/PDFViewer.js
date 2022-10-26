b1044da0eb89e0acd3a40745768289fcd7c5c566
Priscilla Deng <priscilla.deng@mail.utoronto.ca>
2022-10-25 20:55:46 -0400

    Added pdf-extract functionality to react-app

--- clippy/src/components/PDFViewer/PDFViewer.js
+++ clippy/src/components/PDFViewer/PDFViewer.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import * as pdfjsLib from '../../../public/build/pdf';
+pdfjsLib.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjsLib.version}/pdf.worker.js`;
+
+
 
 export default class PDFViewer extends React.Component {
   constructor(props) {
     super(props);
     this.viewerRef = React.createRef();
     this.backend = new props.backend();
+    this.PDFToText = this.PDFToText.bind(this);
   }
 
   componentDidMount() {
@@ -14,10 +19,22 @@ export default class PDFViewer extends React.Component {
     this.backend.init(src, element);
   }
   
+  async PDFToText() {
+    console.log('meh');
+    let doc = await pdfjsLib.getDocument('./pdf-test.pdf').promise;
+    let pageTexts = Array.from({length: doc.numPages}, async (v,i) => {
+        return (await (await doc.getPage(i+1)).getTextContent()).items.map(token => token.str).join('');
+    });
+    let result= (await Promise.all(pageTexts)).join('');
+    console.log(result);
+  }
+
   render() {
     return (
-      <div ref={this.viewerRef} id='viewer' style={{ width: '100%', height: '100%' }}>
-
+      <div ref={this.viewerRef} id='viewer' style={{ width: '100%', height: '100%' }} >
+      <button onClick={this.PDFToText}> 
+        bleh
+      </button>
       </div>
     )
   }
