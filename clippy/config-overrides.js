a7d86add0b8a4ec807460fccc28d95e58b701937
Priscilla Deng <priscilla.deng@mail.utoronto.ca>
2022-11-06 20:10:17 -0500

    Button click will extract pdf text and call summarize API

--- clippy/config-overrides.js
+++ clippy/config-overrides.js
@@ -3,6 +3,11 @@ const webpack = require('webpack');
 
 module.exports = function override(config, env) {
     config.resolve.plugins = config.resolve.plugins.filter(plugin => !(plugin instanceof ModuleScopePlugin));
+    resolve: {
+        alias: {
+        process: "process/browser"
+      }
+    }
     const fallback = config.resolve.fallback || {};
     Object.assign(fallback, {
         "crypto": require.resolve("crypto-browserify"),
@@ -14,7 +19,8 @@ module.exports = function override(config, env) {
         "url": require.resolve("url"),
         "zlib": require.resolve("browserify-zlib"),
         "fs": require.resolve("browserify-fs"),
-        "path": require.resolve("path-browserify")
+        "path": require.resolve("path-browserify"),
+        "process/browser": require.resolve("process/browser")
     })
     config.resolve.fallback = fallback;
     config.plugins = (config.plugins || []).concat([
--- clippy/public/web/clippy.js
+++ clippy/public/web/clippy.js
@@ -1,27 +1,11 @@
-import React, { useEffect } from 'react';
+import * as pdfjsLib from '../build/pdf';
 import axios from 'axios';
-import * as pdfjsLib from '../../../public/build/pdf';
 require('dotenv').config();
 pdfjsLib.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjsLib.version}/pdf.worker.js`;
 
+document.getElementById("summary").addEventListener("click", PDFToText);
 
-
-export default class PDFViewer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.viewerRef = React.createRef();
-    this.backend = new props.backend();
-    this.PDFToText = this.PDFToText.bind(this);
-  }
-
-  componentDidMount() {
-    const { src } = this.props;
-    const element = this.viewerRef.current;
-
-    this.backend.init(src, element);
-  }
-  
-  async PDFToText() {
+async function PDFToText() {
     let doc = await pdfjsLib.getDocument('./pdf-test.pdf').promise;
     let pageTexts = Array.from({length: doc.numPages}, async (v,i) => {
         return (await (await doc.getPage(i+1)).getTextContent()).items.map(token => token.str).join(' ');
@@ -41,13 +25,3 @@ export default class PDFViewer extends React.Component {
     .catch(error => console.log('error', error))
   }
 
-  render() {
-    return (
-      <div ref={this.viewerRef} id='viewer' style={{ width: '100%', height: '100%' }} >
-      <button onClick={this.PDFToText}> 
-        bleh
-      </button>
-      </div>
-    )
-  }
-}
