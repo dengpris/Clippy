c52fd25f95b9229081445b659e24a3be96707ef3
bethany-c <70467659+bethany-c@users.noreply.github.com>
2022-11-30 21:52:57 -0700

    Update Sidebar.js

--- src/components/viewerComponents/Sidebar.js
+++ src/components/viewerComponents/Sidebar.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import './viewerComponents.css';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
 
 const Sidebar = (props) => {
+    const {
+        summary,
+        hideSidebar
+    } = props;
     return(
         <div className="sidebar">
-            <p>{props.summary}</p>
-            <button id="close">&times; close</button>
+            <p>{ summary }</p>
+            <Button 
+              id="close"
+              onClick={() => { hideSidebar }}
+            >Close
+            </Button>
         </div>
     )
+};
+
+Sidebar.propTypes = {
+    summary: PropTypes.string,
+    hideSidebar: PropTypes.func
 }
 
-export default Sidebar
+export default Sidebar;
