a2162ec97bda2f8d5d8f9e6b8da336f27606fe5b
MJ Cuevas <cuevas.mariejoy@gmail.com>
2023-02-10 01:36:07 -0500

    NOT TESTED: still trying to setup the organization by arrows

--- src/components/citationMapping/VisualizeGraph.js
+++ src/components/citationMapping/VisualizeGraph.js
@@ -29,7 +29,9 @@ const VisualizeGraph = () => {
   const [showModal, setShowModal] = useState(false);
   const [defaultDoi, setDefaultDoi] = useState(null);
   const [loading, setLoading] = useState(true);
-
+  const [fosColours, setFosColours] = useState({});
+  const [fosToDoi, setFosToDoi] = useState({});
+  
 
   //MJ Idek what useEffect does but it looks like we need the find_citations in useEffect. Idek why it's getting called 3 other times tho.
   useEffect(() => {
@@ -56,7 +58,38 @@ const VisualizeGraph = () => {
 /*     let newAbsFosInfo = {};
     newAbsFosInfo[Object.keys(citationInfo)[0]] = Object.values(abstractFosInfo)[0];
     console.log(newAbsFosInfo); */
-    console.log("hi");
+    /* var fosColours_setup = {
+      "Computer Science" : "ff0000",
+      "Medicine" : "edb9b9",
+      //"Chemistry" :
+      "Biology" : "8f2323",
+      // "Materials Science" :
+      // "Physics" :
+      "Geology" : "ff7f00",
+      // "Psychology" :
+      // "Art" :
+      // "History" :
+      "Geography" : "ff7f00",
+      "Sociology" : "6b238f",
+      //"Business" :
+      "Political Science" : "6aff00",
+      // "Economics" :
+      // "Philosophy" :
+      // "Mathematics" :
+      // "Engineering" :
+      "Environmental Science" : "bfff00"
+      // "Agricultural and Food Sciences" :
+      // "Education" :
+      // "Law" :
+      // "Linguistics" : 
+    }; */
+
+    var fosColours_setup = {
+      ComputerScience : "ff0000",
+      Medicine : "edb9b9"
+    };
+    setFosColours(fosColours_setup);
+    console.log(fosColours_setup);
   }
 
   const getNodes = () => { // creates the node in the graph
@@ -87,7 +120,7 @@ const VisualizeGraph = () => {
     
     //console.log(citationInfo["10.1002/fee.1950"]);
     //console.log(abstractFosInfo[Object.keys(citationInfo)[0]].fos[0]);
-    
+    var fosToDoi_setup = {};
     //Note that it only compares to the FIRST fosOrig. This is assuming it only has ONE fos.
     var fosOrig = "";
     if(abstractFosInfo[defaultDoi].fos != null){
@@ -105,6 +138,15 @@ const VisualizeGraph = () => {
         // haven't checked this cuz currently no citations that satisfy this case
         field = abstractFosInfo[Object.keys(citationInfo)[i]].fos[0];
         for(let j = 0 ; j< abstractFosInfo[Object.keys(citationInfo)[i]].fos.length ; j++){
+          var currField = abstractFosInfo[Object.keys(citationInfo)[i]].fos[j]
+          if (!Object.keys(fosToDoi_setup).includes(currField)){
+            console.log(currField);
+            fosToDoi_setup[currField] = [];
+          }
+          if (!fosToDoi_setup[currField].includes(Object.keys(citationInfo)[i])){
+          console.log("field: ", currField, " id: ", Object.keys(citationInfo)[i]);
+          fosToDoi_setup[currField].push(Object.keys(citationInfo)[i]);
+          }
           // will work even if fosOrig doesn't exist. but if it doesn't exist, then we're unecessarily going through these.
           if (fosOrig == abstractFosInfo[Object.keys(citationInfo)[i]].fos[j]){
             field = fosOrig;
@@ -113,11 +155,12 @@ const VisualizeGraph = () => {
         }
         
       }
-      
+
       var defaultEdge = {
         from: defaultDoi,
         to: Object.keys(citationInfo)[i],
-        label: field
+        label: field,
+        hidden: false
       };
       graphEdges.push(defaultEdge);
     
@@ -136,22 +179,24 @@ const VisualizeGraph = () => {
           }
   
           field = abstractFosInfo[Object.values(citationInfo)[i].connected_refs[j]].fos[0];
-          console.log(field);
           for(let k = 0 ; k< abstractFosInfo[Object.values(citationInfo)[i].connected_refs[j]].fos.length ; k++){
             if (fosCurr == abstractFosInfo[Object.values(citationInfo)[i].connected_refs[j]].fos[k]){
-              console.log(Object.values(citationInfo)[i].connected_refs[j]);
+              //console.log(Object.values(citationInfo)[i].connected_refs[j]);
               field = fosCurr;
               break;
             }
           }
         }
         var tmpEdge = {};
+        //tmpEdge.id = "nextLay_"+tmpFrom;
         tmpEdge.from = tmpFrom;
         tmpEdge.to = Object.values(citationInfo)[i].connected_refs[j];
         tmpEdge.label = field;
         graphEdges.push(tmpEdge);
       }
     }
+    console.log(fosToDoi_setup);
+    setFosToDoi(fosToDoi_setup);
     
 
     // Loop through all that have more than one fOS
@@ -199,20 +244,49 @@ const VisualizeGraph = () => {
   const events = {
     select: function(event) {
       var { nodes, edges } = event;
+      console.log(event);
+      //toggleEdgeView(event,edges);
+      
     }
   };
+
+  function toggleEdgeView (event, selected_edges) {
+    console.log("nodes updating");
+    var currEdges = graph.edges;
+    var newEdges = selected_edges;
+    console.log(currEdges);
+
+    for (var i = 0; i < currEdges.length; i++) {
+      for (var j = 0; j < newEdges.length; j++) {
+        if (currEdges[i].id == newEdges[j]) {
+          console.log(currEdges[i].hidden);
+          console.log(currEdges[i].id);
+          if (currEdges[i].hidden == true){
+            currEdges[i].hidden = false;
+          }else{
+            currEdges[i].hidden = true;
+          }
+          currEdges[i].color = "#5151fc";
+          //delete currEdges[i];
+          
+        }
+      }
+    }
+    setEdges(currEdges);
+    console.log(graph.edges);
+    console.log("nodes updates");
+  }
 
   const renderConditionalGraph = () => {
     if(!loading) {
       return (
         <Graph
+          //getNetwork={network => this.setState({ network })}
           key={ uuidv4() } // need to generate unique key for graph each render
           graph={graph}
           options={options}
           events={events}
-          // getNetwork={network => {
-          //   //  if you want access to vis.js network api you can set the state in a parent component using this property
-          // }}
+          
         />
       )
     } else {
