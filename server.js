b59f7e60db4c0980ae1dff82a262a01400532892
Hannah Ruiz <hcruiz0211@gmail.com>
2023-03-02 20:20:27 -0500

    Put title and body text in dictionary

--- server.js
+++ server.js
@@ -26,10 +26,10 @@ const server = http.createServer((req, res) => {
       break;
     }
   }
-  console.log(user_file);
+  //console.log(user_file);
   const readline = require('readline');
   var file = dir_path.concat(user_file);
-  console.log(file);
+  //console.log(file);
 
   var text_dict = [];
   var title_array = [];
@@ -39,47 +39,47 @@ const server = http.createServer((req, res) => {
     input: fs.createReadStream(file)
   });
   r.on('line', function(text){
-   console.log("Line: ", text);
+   //console.log("Line: ", text);
    
    if(text.match(/<zone label="*(.*)<\/zone>/)){
-    console.log("Line 2: ", text);
+    //console.log("Line 2: ", text);
     test = text.match(/<zone label="*(.*)<\/zone>/);
-    console.log("Captured Title:", test[1]);
+    //console.log("Captured Title:", test[1]);
     input_text = test[1].match(/(.*)">(.*)/)
-    console.log("Heading:", input_text[1].trim(), "Text:",input_text[2].trim());
+    //console.log("Heading:", input_text[1].trim(), "Text:",input_text[2].trim());
     heading_text = input_text[1].trim();
     lastHeading = heading_text;
     if(heading_text == "BODY_CONTENT"){
       text_dict.push(input_text[2].trim());
-      console.log(JSON.stringify(text_dict));
+      //console.log(JSON.stringify(text_dict));
     }
     if(heading_text == "MET_TITLE"){
       title_array.push(input_text[2].trim());
-      console.log(JSON.stringify(title_array));
+      //console.log(JSON.stringify(title_array));
     }
     //zone_array.push()
    }
    else if(text.match(/<zone label="*(.*)/)){
     test = text.match(/<zone label="*(.*)/);
-    console.log("Captured Title_Continue:", test[1]);
+    //console.log("Captured Title_Continue:", test[1]);
     input_text = test[1].match(/(.*)">(.*)/)
-    console.log("Heading:", input_text[1].trim(), "Text:",input_text[2].trim());
+    //console.log("Heading:", input_text[1].trim(), "Text:",input_text[2].trim());
     heading_text = input_text[1].trim();
     lastHeading = heading_text;
     if(heading_text == "BODY_CONTENT"){
       text_dict.push(input_text[2].trim());
-      console.log(JSON.stringify(text_dict));
+      //console.log(JSON.stringify(text_dict));
     }
     if(heading_text == "MET_TITLE"){
       title_array.push(input_text[2].trim());
-      console.log(JSON.stringify(title_array));
+      //console.log(JSON.stringify(title_array));
     }
    }
    else if ((text.match(/<document>/)) || (text.match(/<\/document>/))){
-    console.log("Skip!");
+    //console.log("Skip!");
    }
    else{
-    console.log("Captured Line_From_Continue: ", text);
+    //console.log("Captured Line_From_Continue: ", text);
     input_text = " ".concat(text);
     if (input_text.match(/(.*)<\/zone>/)){
       test = text.match(/(.*)<\/zone>/);
@@ -89,17 +89,25 @@ const server = http.createServer((req, res) => {
       last_body_content = text_dict[text_dict.length-1];
       last_body_content = last_body_content.concat(" ");
       text_dict[text_dict.length-1] = last_body_content.concat(input_text);
-      console.log(JSON.stringify(text_dict));
+      //console.log(JSON.stringify(text_dict));
     }
     if(lastHeading == "MET_TITLE"){
       last_title_content = title_array[title_array.length-1];
       last_title_content = last_title_content.concat(" ");
       title_array[title_array.length-1] = last_title_content.concat(input_text);
-      console.log(JSON.stringify(title_array));
+      //console.log(JSON.stringify(title_array));
     }
    }
+   console.log(JSON.stringify(text_dict));
    console.log(JSON.stringify(title_array));
+
+   pdf_info = {};
+   pdf_info["TITLE"] = title_array[0];
+   pdf_info["BODY_CONTENT"] = text_dict.join("");
+   console.log(JSON.stringify(pdf_info));
+
   });
+  return [title_array, text_dict];
 
 });
 
